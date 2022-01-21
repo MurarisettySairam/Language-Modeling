32376b048fea6db28537c8d28c9398c8055b8828
sai-1999-ram <sram3917@gmail.com>
2022-01-21 21:26:02 +0530

    Compute Bigram Probabilities

--- language.py
+++ language.py
@@ -158,7 +158,19 @@ Parameters: dict mapping strs to ints ; dict mapping strs to (dicts mapping strs
 Returns: dict mapping strs to (dicts mapping strs to (lists of values))
 '''
 def buildBigramProbs(unigramCounts, bigramCounts):
-    return
+    d={}      
+    for i in bigramCounts:
+        l=unigramCounts[i]
+        l2=[]
+        l3=[]
+        for j in bigramCounts[i]:
+            l2.append(j)
+            l3.append(bigramCounts[i][j]/l)
+        d1={}
+        d1["words"]=l2
+        d1["probs"]=l3
+        d[i]=d1
+    return d
 
 
 '''
@@ -346,7 +358,8 @@ if __name__ == "__main__":
     # test.testCountStartWords()
     # test.testCountBigrams()
     # test.testBuildUniformProbs()
-    test.testBuildUnigramProbs()
+    # test.testBuildUnigramProbs()
+    test.testBuildBigramProbs()
     ## Uncomment these for Week 2 ##
 """
     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
